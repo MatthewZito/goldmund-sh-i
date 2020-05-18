40870a2185b71fe34a1a9c584b3a5e3058c4eb78
Goldmund <matthewtzito@gmail.com>
2020-05-17 20:20:22 -0700

    add dockerfile

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.CLOUD_MONGO, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
   })
