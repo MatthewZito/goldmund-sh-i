16efcf0736314f60a04182ae0140f90b087e8ebe
Goldmund <matthewtzito@gmail.com>
2020-05-30 09:13:00 -0700

    add template reset to deauth

--- packages/cli/commands/deauth.js
+++ packages/cli/commands/deauth.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const chalk = require("chalk");
-const { authorize, readToken } = require("../utils/fs.js");
+const { authorize, readToken, persist } = require("../utils/fs.js");
 const { logout } = require("../utils/requests.js");
 
 /**
@@ -11,14 +11,24 @@ const destroyAuth = async () => {
         const { token } = readToken();
         if (!token || token === "") {
             throw new Error("[-] Unable to find local session.\n");
+            
         }
         let response = logout(token);
         if (!response || token === "") {
             throw new Error("[-] Unable to destroy local session.\n");
         }
         else {
+            let ephemeralEntryTemplate = {}
+            const fields = [ "deleted", "tags", "title", "subtitle", "imgsrc", "content"]
+            fields.forEach(field => 
+                field === "tags" ? ephemeralEntryTemplate[field] = [] : ephemeralEntryTemplate[field] = ""
+                );
+            // depopulate local tempfile
+            persist(ephemeralEntryTemplate);
+            console.log(chalk.green("[+] Successfully cleared entry template.\n"));
+            // destroy local token
             authorize({ token: "" });
-            console.log(chalk.green("[+] Successfully destroyed current session.\n"))
+            console.log(chalk.green("[+] Successfully destroyed current session.\n"));
         }
     } catch(err) {
         console.log(chalk.red(err));
