bb2fdd70f156ef035c0490a8109374a778f954b1
Goldmund <matthewtzito@gmail.com>
2020-06-07 12:27:02 -0700

    finalize nginx mock-ingress

--- packages/goldmund-cli/config/config.js
+++ packages/goldmund-cli/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
     localStore: "tmp/ephemera.json",
     sessionStore: "tmp/session.json",
-    basePath: process.env.DPS_BASE_URL || "http://localhost:5000",
+    basePath: process.env.DPS_BASE_URL || "http://localhost:80/api",
     email: process.env.CLI_EMAIL,
     password: process.env.CLI_PASSWORD
 }
